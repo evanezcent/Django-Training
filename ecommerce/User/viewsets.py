7b9d3a7b534c1d9f562e549bcf7a638da98447e3
Pratama Yoga <pratamayo16@gmail.com>
2020-09-06 19:14:16 +0700

    .

--- ecommerce/User/viewsets.py
+++ ecommerce/User/viewsets.py
@@ -7,13 +7,3 @@ from rest_framework.authtoken.serializers import AuthTokenSerializer
 class UserViewset(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-class LoginAPI():
-    permission_classes = (permissions.AllowAny,)
-
-    def post(self, request, format=None):
-        serializer = AuthTokenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        login(request, user)
-        return super(LoginAPI, self).post(request, format=None)
